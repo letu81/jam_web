965c9c0497bc0465820c933698e3745bacc368fa
tuda2008 <1756991323@qq.com>
2018-08-09 09:51:36 +0800

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -90,8 +90,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   
+  require 'sidekiq/web'
   authenticate :admin_user do
-    mount Sidekiq => '/sidekiq'
+    mount Sidekiq::Web => '/sidekiq'
   end
   
   mount GrapeSwaggerRails::Engine => '/apidoc'
