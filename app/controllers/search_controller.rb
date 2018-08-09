e2a50ed7b884bcc4668bda9d7e6353479714b118
tuda2008 <1756991323@qq.com>
2018-08-09 09:44:14 +0800

    minor changes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,19 +3,4 @@ class SearchController < ApplicationController
   def query
     render layout: false
   end
-
-  def password
-  	p "========pppppppp"
-  	num = 0
-  	while num < 100000
-  	  num = rand(999999)
-    end
-    @key = params[:code].blank? ? num : params[:code]
-	@val = Digest::MD5.hexdigest("raymart#{@key}")
-	@date = Time.new.strftime("%Y-%m-%d-%H")
-	@minute = Time.new.strftime("%M").to_i
-	@value = Digest::MD5.hexdigest("#{@val}#{@date}-#{@minute/5}")
-	@d_password = @value.scan(/\d/).first(6).join('')
-	render layout: false
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
   get 'faq' => 'home#faq', as: :faq
   get 'partners' =>'home#partners', as: :partners
   get 'wap' => 'home#wap', as: :wap
-  get 'test' => 'home#test', as: :test
   get 'privacy' => 'home#privacy', as: :privacy
   get 'app' => 'home#app', as: :app
 
@@ -40,7 +39,6 @@ Rails.application.routes.draw do
   end
 
   get 'search' => 'search#query', as: :search
-  get '/lock/password' => 'search#password', as: :lock_password
 
   ######################### 微信公众平台开发 ###########################
   post '/wechat' => 'weixin/home#welcome', defaults: { format: 'xml' }
@@ -92,9 +90,8 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   
-  require 'sidekiq/web'
   authenticate :admin_user do
-    mount Sidekiq::Web => 'sidekiq'
+    mount Sidekiq => '/sidekiq'
   end
   
   mount GrapeSwaggerRails::Engine => '/apidoc'
